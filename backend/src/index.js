83de02359791d33f42d916a027e02796721449a8
Marelo104 <yanninckaltay@gmail.com>
2026-05-12 11:03:18 +0100

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,10 +24,10 @@ const PORT = process.env.PORT || 8000
 const __dirname = path.resolve();
 
 // middleware
-app.use(cors({
-  origin: 'https://music-streaming-app-y8hs.onrender.com',
-  credentials: true
-}));
+// app.use(cors({
+//   origin: 'https://music-streaming-app-y8hs.onrender.com',
+//   credentials: true
+// }));
 
 app.use(express.json());
 app.use(cookieParser());
